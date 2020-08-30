cdfcc351054176615e3ce8f042a83d412a1a31c5
TimHuynh0905 <timhuynh0905@gmail.com>
2020-08-29 22:39:06 -0500

    box-shadow for card components and added companies logo

--- src/components/OurPartners/OurPartners.js
+++ src/components/OurPartners/OurPartners.js
@@ -8,10 +8,10 @@ const OurPartners = (props) => (
     <div className="container">
         <h3 className="style">OUR PARTNERS</h3>
         <div className="row values">
-            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/brand-1.svg" alt="value" id="value" /></div>
-            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/brand-2.svg" alt="value" id="value" /></div>
-            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/brand-3.svg" alt="value" id="value" /></div>
-            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/brand-4.svg" alt="value" id="value" /></div>
+            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/logo-adidas.png" alt="value" id="value" /></div>
+            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/logo-nike.png" alt="value" id="value"/></div>
+            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/logo-reebok.png" alt="value" id="value" /></div>
+            <div className="d-flex justify-content-center col-lg-3 col-md-3 col-sm-3 col-4 my-3"><img src="assets/images/shoes-img/logo-puma.png" alt="value" id="value" /></div>
         </div>
         <h3 className="style">OUR VALUES</h3>
         <div className="row values">
