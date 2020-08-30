95e60b4663ea5bd4502493d40e4c829c9b97313e
TimHuynh0905 <timhuynh0905@gmail.com>
2020-08-30 15:55:34 -0500

    navbar style changes on scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,16 +2,29 @@ import React, {useState} from 'react';
 import { Navbar, Nav, NavDropdown, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { LinkContainer } from "react-router-bootstrap";
-import logo from '../../vigg.png';
+import logo_black from '../../vigg_black.png';
+import logo_white from '../../vigg.png';
 import './Navbar.css';
 
 const NavBar = () => {
     const [loggedIn, setLoggedIn] = useState(true);
+    const [navbar, setNavbar] = useState(false); 
+
+    const changeBackground = () => {
+        if (window.scrollY >= 70) {
+            setNavbar(true);
+        } else {
+            setNavbar(false);
+        }
+    }
+
+    window.addEventListener('scroll', changeBackground);
+
     return (
-        <Navbar fluid="true" collapseOnSelect fixed="top" className="navbar-color" expand='lg'>
+        <Navbar fluid="true" collapseOnSelect fixed="top" className={navbar ? 'navbar active' : 'navbar'} expand='lg'>
             <Navbar.Brand>
                 <LinkContainer to="/">
-                    <img alt="Vigg Icon" src={logo} className="navbar-brand"/>
+                    <img alt="Vigg Icon" src={navbar ? logo_white : logo_black} className="navbar-brand"/>
                 </LinkContainer>
             </Navbar.Brand>
             <Navbar.Toggle>
@@ -24,9 +37,9 @@ const NavBar = () => {
                     {/* <NavItem className="nav-text">
                         <Link to="/about-us" style={{color: 'white', textDecoration: 'none'}}> ABOUT US </Link>
                     </NavItem> */}
-                    <NavItem className="nav-text">
-                        <Link to="/shop" style={{color: 'white', textDecoration: 'none'}}> SHOP </Link>
-                    </NavItem>
+                    <Nav.Item className="nav-text">
+                        <Link to="/shop" style={{textDecoration: 'none'}}> SHOP </Link>
+                    </Nav.Item>
                     <NavDropdown alignRight title="SELL WITH US">
                         <LinkContainer to="/sell-now">
                             <NavDropdown.Item>Sell now</NavDropdown.Item>
@@ -57,7 +70,7 @@ const NavBar = () => {
                     </LinkContainer> */}
                     <LinkContainer to="/cart">
                         <div className="nav-icon">
-                            <svg className="bi bi-cart-plus" width="4em" height="4em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg" style={{padding:'0px 20px 0px 20px'}}>
+                            <svg className="bi bi-cart-plus" width="4em" height="4em" viewBox="0 0 16 16" fill={navbar ? "white" : "black"} xmlns="http://www.w3.org/2000/svg" style={{padding:'0px 20px 0px 20px'}}>
                                 <path fillRule="evenodd" d="M8.5 5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 .5-.5z"/>
                                 <path fillRule="evenodd" d="M8 7.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0v-2z"/>
                                 <path fillRule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -68,7 +81,7 @@ const NavBar = () => {
                         loggedIn ?
                         <LinkContainer to="/profile">
                             <div className="nav-icon">
-                                <svg className="bi bi-person-circle" width="4em" height="4em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg" style={{padding:'0px 20px 0px 20px'}}>
+                                <svg className="bi bi-person-circle" width="4em" height="4em" viewBox="0 0 16 16" fill={navbar ? "white" : "black"} xmlns="http://www.w3.org/2000/svg" style={{padding:'0px 20px 0px 20px'}}>
                                     <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
                                     <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                     <path fillRule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
