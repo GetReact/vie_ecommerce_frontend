15dfa66e6cac463d8b02ee203ca7e75b18025bfe
TimHuynh0905 <timhuynh0905@gmail.com>
2020-10-11 17:19:31 -0500

    created SlickCarousel component

---
+++ src/components/HomeCarousel/SlickCarousel.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Slider from 'react-slick';
+import ArrowLeft from "@material-ui/icons/ArrowLeft";
+import ArrowRight from "@material-ui/icons/ArrowRight";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Card from '../Card/Card';
+import "./SlickCarousel.css";
+
+class SlickCarousel extends Component  {
+
+    state = {
+        shoesItems: [
+            {
+                title: 'shoes1',
+                imageUrl: 'assets/images/shoes-img/shoes1.jpg',
+            },
+            {
+                title: 'shoes2',
+                imageUrl: 'assets/images/shoes-img/shoes2.jpg',
+            },
+            {
+                title: 'shoes3',
+                imageUrl: 'assets/images/shoes-img/shoes3.jpg',
+            },
+            {
+                title: 'shoes4',
+                imageUrl: 'assets/images/shoes-img/shoes4.jpg',
+            },
+            {
+                title: 'shoes5',
+                imageUrl: 'assets/images/shoes-img/shoes1.jpg',
+            },
+            {
+                title: 'shoes6',
+                imageUrl: 'assets/images/shoes-img/shoes2.jpg',
+            },
+        ]
+    }
+
+    render() {
+        const settings = {
+            infinite: false,
+            centerPadding: '0',
+            slidesToShow: (this.props.size === 'large' ? 3 : 4),
+            swipeToSlide: true,
+            dots: true,
+            pauseOnHover: true,
+            autoplay: true,
+            speed: 300,
+            nextArrow: <ArrowRight/>,
+            prevArrow: <ArrowLeft/>,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 765,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 575,
+                    settings: {
+                        slidesToShow: 2,
+                        arrows: false,
+                    }
+                },
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false,
+                    }
+                }
+            ]
+        }
+
+        return (
+            <Container>
+                <h3 className="style mb-3" onClick = {() => this.props.history.push("/shop")}>
+                    {this.props.name.toUpperCase()}
+                </h3>
+                <Slider {...settings} className="slider-carousel">
+                    {this.state.shoesItems.map(({imageUrl, ...otherSectionProps }) => (
+                        <Card img={imageUrl} {...otherSectionProps}/>
+                    ))}
+                </Slider>
+            </Container>
+        )
+    
+    }
+}
+
+export default withRouter(SlickCarousel);
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Carousel from '../components/Carousel/Carousel';
-import NewReleases from '../components/HomeCarousel/NewReleases';
-import BestSellers from '../components/HomeCarousel/BestSellers';
+import SlickCarousel from '../components/HomeCarousel/SlickCarousel';
 import OurPartners from '../components/OurPartners/OurPartners';
 
 const Home = (props) => (
     <div role="main">
         <Carousel />
-        <NewReleases />
-        <BestSellers />
+        <SlickCarousel 
+            size = "large"
+            name = "NEW RELEASE"
+        />
+        <SlickCarousel 
+            size = "small"
+            name = "BEST SELLERS"
+        />
         <OurPartners />
     </div>
 );
