1562e43725bcc728e415679f0b05cecb101df887
TimHuynh0905 <timhuynh0905@gmail.com>
2020-10-17 13:58:09 -0500

    add items into cart dropdown

--- src/components/CartDropDown/CartDropDown.js
+++ src/components/CartDropDown/CartDropDown.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
 import './CartDropDown.css';
+import { connect } from 'react-redux';
+import CartItem from '../CartItem/CartItem';
+const CartDropDown = ({ cartItems, history }) => {
 
-const CartDropDown = (props) => (
-    <div className='cart-dropdown'>
-        <h6 className='dropdown-title'>MOST RECENT ITEM</h6>
-        <div className='cart-items'/>
-        <Button
-            onClick={ () => {props.history.push('/cart')} } 
-            variant='outline-dark'
-            size="sm">
-                VIEW CART ({0})
-        </Button>
-    </div>
-);
+    let cartLength = 0;
+    cartItems.map(item => cartLength += item.quantity);
+    
+    return (
+        <div className='cart-dropdown'>
+            <h6 className='dropdown-title'>CART SUMMARY</h6>
+            <div className='cart-items'>
+                { 
+                    cartItems.length > 0 
+                    ? (
+                        cartItems.map(item => 
+                            <CartItem key={item.id} item={item} />
+                        )
+                    )
+                    : ""
+                }
+            </div>
+            <Button
+                onClick={ () => {history.push('/cart')} } 
+                variant='outline-dark'
+                size="sm">
+                    VIEW CART ({cartLength})
+            </Button>
+        </div>
+    )
+};
 
-export default withRouter(CartDropDown);
+const mapStatetoProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+export default withRouter(connect(mapStatetoProps)(CartDropDown));
