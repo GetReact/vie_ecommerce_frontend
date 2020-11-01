91013a02761643ba2db7028eb7eb715947c3febe
TimHuynh0905 <timhuynh0905@gmail.com>
2020-11-01 15:33:48 -0600

    make shoppage a function component

--- src/containers/ShopPage/ShopPage.js
+++ src/containers/ShopPage/ShopPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -7,65 +7,54 @@ import ProductGrid from '../../components/ProductGrid/ProductGrid';
 import { selectShopCollection } from '../../redux/shop/shop-selectors';
 import "./ShopPage.css";
 
-class ShopPage extends Component {
+const ShopPage = (props) => {
+    const [ width, setWidth ] = useState(window.innerWidth);
 
-    state = {
-        width: window.innerWidth,
+    const handleWindowSizeChange = () => {
+        setWidth(window.innerWidth);
     }
 
-    handleWindowSizeChange() {
-        this.setState({ width: window.innerWidth });
-    }
+    window.addEventListener('resize', handleWindowSizeChange)
 
-    componentDidMount() {
-        window.addEventListener('resize', this.handleWindowSizeChange.bind(this))
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleWindowSizeChange.bind(this))
-    }
-
-    render() {
-        return (
-            <div className="productpage">
-                {   
-                    this.state.width > 992 ? (
-                        <>
-                            <Col lg={4} md={12}>
-                                <SideBar/>
-                            </Col>
-                            <Col lg={8} md={12} className="products">
-                                <Row>
-                                    <InputGroup className="search-bar">
-                                        <FormControl
-                                            placeholder="Search"
-                                            aria-label="Search"
-                                            aria-describedby="basic-addon2"
-                                        />
-                                        <InputGroup.Append>
-                                            <Button variant="outline-secondary">Search</Button>
-                                        </InputGroup.Append>
-                                    </InputGroup>
-                                </Row>
-                                <Row className="product-grid">
-                                    <ProductGrid items = { this.props.shoesCollection }/>
-                                </Row>
-                            </Col>
-                        </>
-                    )
-                    : (
-                        <>
+    return (
+        <div className="productpage">
+            {   
+                width > 992 ? (
+                    <>
+                        <Col lg={4} md={12}>
+                            <SideBar/>
+                        </Col>
+                        <Col lg={8} md={12} className="products">
                             <Row>
-                                <Col md={12}><SideBar/></Col>
-                                <Col md={12}><ProductGrid items = { this.props.shoesCollection }/></Col>
+                                <InputGroup className="search-bar">
+                                    <FormControl
+                                        placeholder="Search"
+                                        aria-label="Search"
+                                        aria-describedby="basic-addon2"
+                                    />
+                                    <InputGroup.Append>
+                                        <Button variant="outline-secondary">Search</Button>
+                                    </InputGroup.Append>
+                                </InputGroup>
                             </Row>
-                        </>
-                    )
-                }
+                            <Row className="product-grid">
+                                <ProductGrid items = { props.shoesCollection }/>
+                            </Row>
+                        </Col>
+                    </>
+                )
+                : (
+                    <>
+                        <Row>
+                            <Col md={12}><SideBar/></Col>
+                            <Col md={12}><ProductGrid items = { props.shoesCollection }/></Col>
+                        </Row>
+                    </>
+                )
+            }
 
-            </div>
-        );
-    }
+        </div>
+    );
 };
 
 const mapStatetoProps = createStructuredSelector({
