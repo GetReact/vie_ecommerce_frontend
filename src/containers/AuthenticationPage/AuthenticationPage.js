6ec69a2152099dc69f85e39d232e565920258741
TimHuynh0905 <timhuynh0905@gmail.com>
2020-11-01 16:09:28 -0600

    authentication page becomes function component, memory leak issue fixed

--- src/containers/AuthenticationPage/AuthenticationPage.js
+++ src/containers/AuthenticationPage/AuthenticationPage.js
@@ -1,54 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import './AuthenticationPage.css';
 import LoginForm from './Login';
 import RegisterForm from './Register';
 
-class AuthenticationPage extends Component {
+const AuthenticationPage = (props) => {
+    const [ width, setWidth ] = useState(window.innerWidth);
 
-    state = {
-        windowWidth: window.innerWidth,
-    }
-
-    setWindowWidth() {
-        this.setState({ windowWidth: window.innerWidth })
-    }
-
-    componentDidMount() {
-        window.addEventListener("resize", this.setWindowWidth.bind(this))
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.setWindowWidth.bind(this));
-    }
+    useEffect(() => {
+        const handleWindowSizeChange = () => {
+            if (window.innerWidth !== width) setWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', handleWindowSizeChange);
+        return _ => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    });
     
-    render() {
-        return (
-            <div className="authentication-page">
-                {
-                    this.state.windowWidth > 992 ? (
-                        <>
-                            <Col lg={6}>
-                                <LoginForm/>
-                            </Col>
-                            <Col lg={6}> 
-                                <RegisterForm/>
-                            </Col>
-                        </>
-                    ) : (
-                        <div className="small-page">
-                            <Row>
-                                <LoginForm />
-                            </Row>
-                            <Row>
-                                <RegisterForm />
-                            </Row>
-                        </div>
-                    )
-                }
-            </div>
-        )
-    }
+    return (
+        <div className="authentication-page">
+            {
+                width > 992 ? (
+                    <>
+                        <Col lg={6}>
+                            <LoginForm/>
+                        </Col>
+                        <Col lg={6}> 
+                            <RegisterForm/>
+                        </Col>
+                    </>
+                ) : (
+                    <div className="small-page">
+                        <Row>
+                            <LoginForm />
+                        </Row>
+                        <Row>
+                            <RegisterForm />
+                        </Row>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default AuthenticationPage;
