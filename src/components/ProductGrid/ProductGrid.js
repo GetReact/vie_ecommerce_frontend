9b9bcb90fb86a47265d3e38bf3629f50afc918ad
TimHuynh0905 <timhuynh0905@gmail.com>
2020-11-02 13:26:05 -0600

    product-grid margin added

--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -4,11 +4,19 @@ import { Row, Col } from 'react-bootstrap';
 import './ProductGrid.css';
 
 const ProductGrid = (props) => (
-    <div className="grid">
-        <Row>
+    <div className='grid'>
+        <Row className='grid-row'>
             {
                 props.items.map((item) => (
-                    <Col key={item.id} xl={4} lg={6} md={6} sm={6}><Card img={item.imageUrl} id={item.id} item={item}/></Col>
+                    <Col 
+                        className='grid-col' 
+                        key={item.id} xl={4} lg={6} md={6} sm={6}>
+                            <Card 
+                                img={item.imageUrl} 
+                                    id={item.id} 
+                                    item={item}
+                            />
+                    </Col>
                 ))
             }
         </Row>
